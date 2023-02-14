789f660d63b417d63c86a02760cf1379f2625b09
ARAJO <fave5820@gmail.com>
2023-02-14 22:01:55 +0900

    2023.02.14, update

--- myapp/views.py
+++ myapp/views.py
@@ -13,8 +13,18 @@ topics = [
             {'id':3, 'title':'model', 'body':'Model is ..'},
 ]
 
-def HTMLTemplate(articleTag):
+def HTMLTemplate(articleTag, id=None):
     global topics
+    contextUI = ''
+    if id != None:
+        contextUI = f'''
+          <li>
+            <form action="/delete/" method="post">
+                <input type="hidden" name="id" value={id}>
+                <input type="submit" value="delete">
+            </form>
+          </li>
+        '''
     ol = ''
     for topic in topics:
         ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
@@ -22,12 +32,13 @@ def HTMLTemplate(articleTag):
     <html>
     <body>
         <h1><a href="/">Django</a></h1>
-        <ol>
+        <ul>
             {ol}
-        </ol>
+        </ul>
         {articleTag}
         <ul>
           <li><a href="/create/">create</a></li>
+          {contextUI}
         </ul>
     </body>
     </html>
@@ -48,7 +59,7 @@ def read(request, id):
     for topic in topics:
         if topic['id'] == int(id):
             article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
-    return HttpResponse(HTMLTemplate(article))
+    return HttpResponse(HTMLTemplate(article, id))
 
 
 @csrf_exempt
@@ -71,4 +82,17 @@ def create(request):
         url = '/read/' + str(nextId)
         nextId = nextId + 1
         return redirect(url)
+
+
+@csrf_exempt
+def delete(request):
+    global topics
+    if request.method == 'POST':
+        id = request.POST['id']
+        newTopics = []
+        for topic in topics:
+            if topic['id'] != int(id):
+                newTopics.append(topic)
+        topics = newTopics
+        return redirect('/')
 
