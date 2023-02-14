1ec633a32a20ab0ccc0470bf77799e1d5cdd3017
ARAJO <fave5820@gmail.com>
2023-02-14 22:00:52 +0900

    2023.02.14, update

--- myapp/views.py
+++ myapp/views.py
@@ -2,10 +2,11 @@
 # request(=파라미터) : 파라미터의 인자로 요청과 관련된 여러가지 정보가 들어오도록 약속되어있는 객체를 전달해주도록 되어있음 / 파라미터 이름은 아무거나 상관없지만, 관습적으로 request를 사용함
 # 우리가 처리한 결과를 client로 보내줄 때 return값으로 보내주는데, Http를 이용해서 응답하겠다는 의미에서 HttpResponse라는 객체를 이용함 / 이 객체를 이용하기 위해서는 import 해줘야함
 
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 import random
 from django.views.decorators.csrf import csrf_exempt
 
+nextId = 4
 topics = [
             {'id':1, 'title':'routing', 'body':'Routing is ..'},
             {'id':2, 'title':'view', 'body':'View is ..'},
@@ -52,12 +53,22 @@ def read(request, id):
 
 @csrf_exempt
 def create(request):
-    article = '''
-    <form action="/create/" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p><textarea name="body" placeholder="body"></textarea></p>
-        <p><input type="submit"></p>
-    </form>
-    '''
-    return HttpResponse(HTMLTemplate(article))
+    global nextId
+    if request.method == 'GET':
+        article = '''
+        <form action="/create/" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p><textarea name="body" placeholder="body"></textarea></p>
+            <p><input type="submit"></p>
+        </form>
+        '''
+        return HttpResponse(HTMLTemplate(article))
+    elif request.method == 'POST':
+        title = request.POST['title']
+        body = request.POST['body']
+        newTopic = {"id":nextId, "title":title, "body":body}
+        topics.append(newTopic)
+        url = '/read/' + str(nextId)
+        nextId = nextId + 1
+        return redirect(url)
 
